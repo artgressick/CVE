7502f2517b2c8d938e5f99b398c1ff8148adbc96
Arthur Gressick <33992993+artgressick@users.noreply.github.com>
2021-05-03 00:44:20 -0400

    Update update.py
    New logic for the arrays

--- update.py
+++ update.py
@@ -24,167 +24,145 @@ def main():
     collection = db['cve_'+file_name]
     cve_data = ''
 
+# ---------------- Switches ------------------------
 # Update Cisco IOS Switches
-    updateResult = collection.update_many({'product': {'$regex':':ios:'}}, {"$set": {'platform':'switch', 'ios':'ios'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:ios:'}}, {"$push": {'vendor':'[cisco]', 'os':'[ios]'}} )
     print("\nUpdating Cisco IOS");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Cisco NX-OS Switches
-    updateResult = collection.update_many({'product': {'$regex':':nx-os:'}}, {"$set": {'platform':'switch', 'nx_os':'nx-os'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:nx-os:'}}, {"$push": {'vendor':'[cisco]', 'os':'[nx-os]'}} )
     print("\nUpdating Cisco NX-OS");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Cisco IOS-XE Switches
-    updateResult = collection.update_many({'product': {'$regex':':ios_xe:'}}, {"$set": {'platform':'switch', 'ios_xe':'ios_xe'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:ios_xe:'}}, {"$push": {'vendor':'[cisco]', 'os':'[ios_xe]'}} )
     print("\nUpdating Cisco IOS XE");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Cisco IOS-XR Switches
-    updateResult = collection.update_many({'product': {'$regex':':ios_xr:'}}, {"$set": {'platform':'switch', 'ios_xr':'ios_xr'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:ios_xr:'}}, {"$push": {'vendor':'[cisco]', 'os':'[ios_xr]'}} )
     print("\nUpdating Cisco IOS XR");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update JUNOS Switches
-    updateResult = collection.update_many({'product': {'$regex':':junos:'}}, {"$set": {'platform':'switch', 'junos':'junos'}} )
+    updateResult = collection.update_many({'product': {'$regex':':juniper:junos:'}}, {"$push": {'vendor':'[juniper]', 'os':'[junos]'}} )
     print("\nUpdating JunOS");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-# Update ARISTA Switches (2)
-    updateResult = collection.update_many({'product': {'$regex':':eos:'}}, {"$set": {'platform':'switch', 'arista':'arista'}} )
+# Update ARISTA Switches
+    updateResult = collection.update_many({'product': {'$regex':':o:arista:'}}, {"$push": {'vendor':'[arista]', 'os':'[eos]'}} )
     print("\nUpdating Arista EOS");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-
-    updateResult = collection.update_many({'product': {'$regex':':extensible_operating_system:'}}, {"$set": {'platform':'switch', 'arista':'arista'}} )
-    print("\nUpdating Arista Extensible Operating System");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
 # Update ARUBA Switches
-    updateResult = collection.update_many({'product': {'$regex':':arubaos:'}}, {"$set": {'platform':'switch', 'aruba':'aruba'}} )
+    updateResult = collection.update_many({'product': {'$regex':':arubaos:'}}, {"$push": {'vendor':'[aruba]', 'os':'[arubaos]'}} )
     print("\nUpdating Aruba");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
+# Update Brocade-Extreme Vyette Routers sold to AT&T
+    updateResult = collection.update_many({'product': {'$regex':':brocade:vyatta'}}, {"$push": {'vendor':'[extreme]', 'os':'[netiron]'}} )
+    print("\nUpdating Extreme");
+    print("Records Matched:", updateResult.matched_count );
+    print("Records Modified:", updateResult.modified_count );
+# Update Brocade-Extreme Netiron Switches
+    updateResult = collection.update_many({'product': {'$regex':':brocade:netiron'}}, {"$push": {'vendor':'[extreme]', 'os':'[netiron]'}} )
+    print("\nUpdating Extreme");
+    print("Records Matched:", updateResult.matched_count );
+    print("Records Modified:", updateResult.modified_count );
+
+# ---------------- NAC ------------------------
 # Update Cisco ISE
-    updateResult = collection.update_many({'product': {'$regex':':identity_services_engine_software:'}}, {"$set": {'platform':'switch', 'cisco_ise':'cisco_ise'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:identity_'}}, {"$push": {'vendor':'[cisco]', 'application':'[cisco_ise]'}} )
     print("\nUpdating Cisco ISE");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Forescout
-    updateResult = collection.update_many({'product': {'$regex':':forescout:'}}, {"$set": {'platform':'switch', 'forescout':'forescout'}} )
+    updateResult = collection.update_many({'product': {'$regex':':forescout:'}}, {"$push": {'vendor':'[forescout]', 'application':'[forescout]'}} )
     print("\nUpdating Forescout");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-
 # Update Clearpass
-    updateResult = collection.update_many({'product': {'$regex':':clearpass:'}}, {"$set": {'platform':'switch', 'clearpass':'clearpass'}} )
-    print("\nUpdating Aruba Clearpass");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
-# Update Clearpass
-    updateResult = collection.update_many({'product': {'$regex':':clearpass_policy_manager:'}}, {"$set": {'platform':'switch', 'clearpass':'clearpass'}} )
+    updateResult = collection.update_many({'product': {'$regex':'arubanetworks:clearpass'}}, {"$push": {'vendor':'[aruba]', 'application':'[clearpass]'}} )
     print("\nUpdating Aruba Clearpass");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Fortinac
-    updateResult = collection.update_many({'product': {'$regex':':fortinac:'}}, {"$set": {'platform':'switch', 'fortinac':'fortinac'}} )
+    updateResult = collection.update_many({'product': {'$regex':':fortinac:'}}, {"$push": {'vendor':'[fortinet]', 'application':'[fortinac]'}} )
     print("\nUpdating Fortinac");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-# Update Cisco ASA Firewall
-    updateResult = collection.update_many({'product': {'$regex':':asa:'}}, {"$set": {'platform':'switch', 'cisco_fw':'cisco_fw'}} )
+
+# ---------------- Firewall ------------------------
+# Update Cisco ASA Firewall (2014 only)
+    updateResult = collection.update_many({'product': {'$regex':':cisco:asa'}}, {"$push": {'vendor':'[cisco]', 'hardware':'[cisco_fw]'}} )
     print("\nUpdating Cisco ASA Firewall");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Cisco ASA Firewall
-    updateResult = collection.update_many({'product': {'$regex':':adaptive_security_appliance:'}}, {"$set": {'platform':'switch', 'cisco_fw':'cisco_fw'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:adaptive_security'}}, {"$push": {'vendor':'[cisco]', 'hardware':'[cisco_fw]'}} )
     print("\nUpdating Cisco ASA Firewall");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Cisco Firepower
-    updateResult = collection.update_many({'product': {'$regex':':firepower:'}}, {"$set": {'platform':'switch', 'cisco_fw':'cisco_fw'}} )
-    print("\nUpdating Cisco Firepower");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
-# Update Cisco Firepower
-    updateResult = collection.update_many({'product': {'$regex':':firepower_threat_defense:'}}, {"$set": {'platform':'switch', 'cisco_fw':'cisco_fw'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:firepower'}}, {"$push": {'vendor':'[cisco]', 'hardware':'[cisco_fw]'}} )
     print("\nUpdating Cisco Firepower");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update PaloAlto
-    updateResult = collection.update_many({'product': {'$regex':':pan-os:'}}, {"$set": {'platform':'switch', 'palo_alto':'palo_alto'}} )
+    updateResult = collection.update_many({'product': {'$regex':':paloaltonetworks:pan-os:'}}, {"$push": {'vendor':'[palo_alto]', 'hardware':'[palo_fw]'}} )
     print("\nUpdating PaloAlto");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Fortinet
-    updateResult = collection.update_many({'product': {'$regex':':fortios:'}}, {"$set": {'platform':'switch', 'fortinet':'fortinet'}} )
+    updateResult = collection.update_many({'product': {'$regex':':fortinet:fortios:'}}, {"$push": {'vendor':'[fortinet]', 'os':'[fortios]', 'hardware':'[fortinet_fw]'}} )
     print("\nUpdating Fortinet");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
 # Update Checkpoint
-    updateResult = collection.update_many({'product': {'$regex':':checkpoint:'}}, {"$set": {'platform':'switch', 'checkpoint':'checkpoint'}} )
+    updateResult = collection.update_many({'product': {'$regex':':checkpoint:'}}, {"$push": {'vendor':'[checkpoint]', 'hardware':'[checkpoint_fw]'}} )
     print("\nUpdating CheckPoint");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
+
+# ---------------- WiFi ------------------------
 # Update Arista Mojo
-    updateResult = collection.update_many({'product': {'$regex':':mojo:'}}, {"$set": {'platform':'switch', 'arista_wifi':'arista_wifi'}} )
+    updateResult = collection.update_many({'product': {'$regex':':mojo:'}}, {"$push": {'vendor':'[arista]', 'hardware':'[arista_wifi]'}} )
     print("\nUpdating Arista Mojo");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
+
 # Update Aruba Wifi
-    updateResult = collection.update_many({'product': {'$regex':':airwave:'}}, {"$set": {'platform':'switch', 'aruba_wifi':'aruba_wifi'}} )
+    updateResult = collection.update_many({'product': {'$regex':':arubanetworks:airwave'}}, {"$push": {'vendor':'[aruba]', 'hardware':'[aruba_wifi]'}} )
     print("\nUpdating Aruba Wifi");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
+
 # Update Aruba Wifi
-    updateResult = collection.update_many({'product': {'$regex':':airwave_network_management:'}}, {"$set": {'platform':'switch', 'aruba_wifi':'aruba_wifi'}} )
+    updateResult = collection.update_many({'product': {'$regex':':arubanetworks:instant'}}, {"$push": {'vendor':'[aruba]', 'hardware':'[aruba_wifi]'}} )
     print("\nUpdating Aruba Wifi");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-# Update Aruba Wifi
-    updateResult = collection.update_many({'product': {'$regex':':web_management_portal:'}}, {"$set": {'platform':'switch', 'aruba_wifi':'aruba_wifi'}} )
-    print("\nUpdating Aruba Wifi");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
-# Update Aruba Wifi
-    updateResult = collection.update_many({'product': {'$regex':':airwave_glass:'}}, {"$set": {'platform':'switch', 'aruba_wifi':'aruba_wifi'}} )
-    print("\nUpdating Aruba Wifi");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
-# Update Aruba Wifi
-    updateResult = collection.update_many({'product': {'$regex':':aruba_instant:'}}, {"$set": {'platform':'switch', 'aruba_wifi':'aruba_wifi'}} )
-    print("\nUpdating Aruba Wifi");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
+
 # Update Cisco Wifi
-    updateResult = collection.update_many({'product': {'$regex':':aironet_access_point_software:'}}, {"$set": {'platform':'switch', 'cisco_wifi':'cisco_wifi'}} )
+    updateResult = collection.update_many({'product': {'$regex':':cisco:aironet'}}, {"$push": {'vendor':'[cisco]', 'hardware':'[cisco_wifi]'}} )
     print("\nUpdating Cisco Wifi");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-# Update Cisco Wifi
-    updateResult = collection.update_many({'product': {'$regex':':aironet_access_point:'}}, {"$set": {'platform':'switch', 'cisco_wifi':'cisco_wifi'}} )
-    print("\nUpdating Cisco Wifi");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
-# Update Cisco Wifi
-    updateResult = collection.update_many({'product': {'$regex':':aironet:'}}, {"$set": {'platform':'switch', 'cisco_wifi':'cisco_wifi'}} )
-    print("\nUpdating Cisco Wifi");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
+
 # Update Extreme Wifi
-    updateResult = collection.update_many({'product': {'$regex':':extremewireless_wing:'}}, {"$set": {'platform':'switch', 'extreme_wifi':'extreme_wifi'}} )
+    updateResult = collection.update_many({'product': {'$regex':':extremenetworks:'}}, {"$push": {'vendor':'[extreme_networks]', 'hardware':'[extreme_wifi]'}} )
     print("\nUpdating Extreme Wifi");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
-# Update Extreme Wifi
-    updateResult = collection.update_many({'product': {'$regex':':extreme_management_center:'}}, {"$set": {'platform':'switch', 'extreme_wifi':'extreme_wifi'}} )
-    print("\nUpdating Extreme Wifi");
-    print("Records Matched:", updateResult.matched_count );
-    print("Records Modified:", updateResult.modified_count );
-# Update Extreme Wifi
-    updateResult = collection.update_many({'product': {'$regex':':aerohive:'}}, {"$set": {'platform':'switch', 'extreme_wifi':'extreme_wifi'}} )
+
+# Update Extreme Wifi (purchased Aerohive)
+    updateResult = collection.update_many({'product': {'$regex':':aerohive:'}}, {"$push": {'vendor':'[extreme_networks]', 'hardware':'[extreme_wifi]'}} )
     print("\nUpdating Aerohive Wifi");
     print("Records Matched:", updateResult.matched_count );
     print("Records Modified:", updateResult.modified_count );
+
+# Meraki products should go here
 
 if __name__ == '__main__':
     return_value = main()
